1edcbffd8c8767e62e3e67c213e900ed5dba48db
Doug Bridgens <github@far-oeuf.com>
2017-03-03 09:33:12 +0000

    add GPL license text

--- firewall.py
+++ firewall.py
@@ -1,5 +1,22 @@
 # file: /usr/lib/python2.7/site-packages/ansible/plugins/strategy/firewall.py
 
+#  A strategy plugin for Ansible (2.2+) to enforce task action restrictions by policy
+#   Copyright (C) 2017  Doug Bridgens, https://github.com/thisdougb
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.   
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
